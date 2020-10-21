5af39e7167ec47e7b2fd517effe692773c88092c
Madsomma <Madsomma@users.noreply.github.com>
2020-10-22 00:35:25 +0200

    lil update, ora runna per 4 ore di notte

--- python-scripts/json-getter.py
+++ python-scripts/json-getter.py
@@ -1,32 +1,51 @@
-from pathlib import Path
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError, URLError
-import pandas as pd
 import time
 
-timestamp1gen2017 = 1483228800000
-timestamp_curr  = int(time.time())
 
-print(timestamp_curr)
+def retrieve_history(currency, log_file):
+    timestamp1gen2017 = 1483228800000
+    timestamp_curr = int(time.time()) * 1000    # to get milliseconds
 
-url = "https://api.coincap.io/v2/assets/bitcoin/history?interval=m1"
+    milliseconds_per_day = 86400000
 
-start = "&start="
+    tmstp = timestamp1gen2017
 
-end = "&end"
+    while tmstp < timestamp_curr:
+        start_day = str(tmstp)
+        end_day = str(tmstp + milliseconds_per_day)
 
-try:
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    response = urlopen(req, timeout=20).read()
-    save_folder = parent_folder / "data/history/"
-    json_save = save_folder / img_name
+        url = "https://api.coincap.io/v2/assets/" + currency + "/history?interval=m1"
 
-    output = open(json_save, "wb")
-    output.write(response)
-    output.close()
+        start = "&start=" + start_day
 
-except (HTTPError, URLError) as e:
-    file.write("Player: " + player_name + " not found. \tReason: " + e.reason + "\n")
-    continue
+        end = "&end=" + end_day
 
-file.close()
+        final_url = url + start + end
+
+        try:
+            req = Request(final_url, headers={'User-Agent': 'Mozilla/5.0'})
+            response = urlopen(req, timeout=700).read()
+            save_folder = "../data/history/" + currency + "/"
+            json_file = save_folder + currency + "-" + start_day + ".json"      # example: bitcoin-1483228800000.json
+
+            output = open(json_file, "wb")
+            output.write(response)
+            output.close()
+
+        except (HTTPError, URLError) as e:
+            log_file.write("Request for currency " + currency + " for day " + start_day + " failed!\tReason: " + e.reason + "\n")
+
+        tmstp += milliseconds_per_day
+
+    log_file.close()
+
+
+# ---------------------------MAIN---------------------------
+
+assets = ["bitcoin", "ethereum", "ripple", "bitcoin-cash", "eos", "stellar", "litecoin", "cardano", "tether", "iota"]
+
+for currency in assets:
+    name_log_file = currency + "_log_file.txt"
+    log_file = open(name_log_file, "w")
+    retrieve_history(currency, log_file)
