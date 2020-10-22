d17c3a4fd219cd25dbdc4c73483bb904ddca266f
Madsomma <Madsomma@users.noreply.github.com>
2020-10-22 15:52:27 +0200

    all csv files

--- python-scripts/json_to_csv.py
+++ python-scripts/json_to_csv.py
@@ -3,40 +3,41 @@ from os import listdir
 from os.path import isfile, join
 
 
-currency = "bitcoin"
+assets = ["ethereum", "tether", "xrp", "litecoin", "cardano", "iota", "eos", "stellar", "bitcoin-cash"]
 
-folder_path = "../data/history/" + currency + "/json/"
+for currency in assets:
 
-json_files = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
+    folder_path = "../data/history/" + currency + "/json/"
 
-# print(json_files)
+    json_files = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
 
-timestamp = []     # in milliseconds
-date = []          # as yyyy-mm-dd
-hour = []          # as hh-mm-ss
-price = []         # cast to float
+    # print(json_files)
 
-cont = 0
+    timestamp = []     # in milliseconds
+    date = []          # as yyyy-mm-dd
+    hour = []          # as hh-mm-ss
+    price = []         # cast to float
 
-for json in json_files:
-    print("For currency " + currency + ", loading is " + str(cont) + "/" + str(len(json_files)))
+    cont = 0
 
-    df = pd.read_json(folder_path + json)["data"].to_list()
+    for json in json_files:
+        print("For currency " + currency + ", loading is " + str(cont) + "/" + str(len(json_files)))
 
-    for minute in df:
-        timestamp.append(minute["time"])
-        price.append(minute["priceUsd"])
-        replace_date = minute["date"].replace(".000Z", "")
-        split_date = replace_date.split("T")
-        date.append(split_date[0])
-        hour.append(split_date[1])
+        df = pd.read_json(folder_path + json)["data"].to_list()
 
-    cont += 1
+        for minute in df:
+            timestamp.append(minute["time"])
+            price.append(minute["priceUsd"])
+            replace_date = minute["date"].replace(".000Z", "")
+            split_date = replace_date.split("T")
+            date.append(split_date[0])
+            hour.append(split_date[1])
 
-df = pd.DataFrame(data={"timestamp": timestamp, "price": price, "date": date, "hour": hour})
-df.to_csv("../data/history/" + currency + "/" + currency + ".csv", sep=',', index=False)
-only_day_df = df[df["hour"] == "00:00:00"]
-only_day_df.to_csv("../data/history/" + currency + "/only_days_" + currency + ".csv", sep=',', index=False)
+        cont += 1
 
-print(df)
-print(only_day_df)
+    df = pd.DataFrame(data={"timestamp": timestamp, "price": price, "date": date, "hour": hour})
+    df.to_csv("../data/history/" + currency + "/" + currency + ".csv", sep=',', index=False)
+    only_day_df = df[df["hour"] == "00:00:00"]
+    only_day_df.to_csv("../data/history/" + currency + "/only_days_" + currency + ".csv", sep=',', index=False)
+
+    print("\n\n------CURRENCY: " + currency + " csv computed!--------\n\n")
